4cc2f78ef7e1aa9468048a88af138dfa1f126aff
Sergei Uversky <suversky@purdue.edu>
2016-04-24 16:50:32 -0400

    checkpoint: simplified structures, attempting to finalize helpInsert

--- BST.cpp
+++ BST.cpp
@@ -21,73 +21,55 @@
 using namespace std;
 
 void BST::initializetree(){
- 
-    tnp t, rootleft, rootright;
-    t.store(new tree_node);
-    rootleft.store(new tree_node);
-    rootright.store(new tree_node);
-    
-    rootleft.load()->data = inf1;
-    rootleft.load()->left.store(nullptr);
-    rootleft.load()->right.store(nullptr);
-    rootleft.load()->isLeaf.store(true);
-    
-    rootright.load()->data = inf2;
-    rootright.load()->left.store(nullptr);
-    rootright.load()->right.store(nullptr);
-    rootright.load()->isLeaf.store(true);
-    
-    t.load()->data = inf2;
-    t.load()->left.store(rootleft);
-    t.load()->right.store(rootright);
-    t.load()->isLeaf.store(false);
-   
-    root.store(t);
+  tnp t, rootleft, rootright;
+  rootleft.store(new treeNode(inf1, true));
+  rootright.store(new treeNode(inf2, true));
+  root.store(new treeNode(inf2, false, rootleft, rootright));
 }
 
 searchResult *
 BST::search(int k)
 {
-    urp pupdate;
-    tnp p,l;
-    p.store(nullptr);
-    pupdate.store(nullptr);
-    l.store(root);
-    do {
-        p.store(l);
-        pupdate.store(p.load()->update);
+  urp pupdate;
+  tnp p,l;
+  p.store(nullptr);
+  pupdate.store(nullptr);
+  l.store(root);
+  do {
+    p.store(l);
+    pupdate.store(p.load()->update);
 
-        if (k < l.load()->data){
-            l.store(p.load()->left);
-        }
-        else{
-            l.store(p.load()->right);
-        }
-    } while(l.load()->isLeaf.load() != true);  
-    searchResult * s = new searchResult;
-    s->p.store(p);
-    s->pupdate.store(pupdate);
-    s->l.store(l);
-    return s;
+    if (k < l.load()->data){
+      l.store(p.load()->left);
+    }
+    else{
+      l.store(p.load()->right);
+    }
+  } while(l.load()->isLeaf != true);  
+  searchResult * s = new searchResult;
+  s->p.store(p);
+  s->pupdate.store(pupdate);
+  s->l.store(l);
+  return s;
 }
 
 void BST::print_preorder()
 {
-    preorder((tnp&)root);
-    cout << endl;
+  preorder((tnp&)root);
+  cout << endl;
 }
 
 void BST::preorder(tnp &p)
 {
-    if(p.load() != NULL)
-    {
-        if (p.load()->isLeaf.load())
-          cout<<" "<<p.load()->data<<" ";
-        if(p.load()->left.load())
-          preorder(p.load()->left);
-        if(p.load()->right.load())
-          preorder(p.load()->right);
-    }
-    else return;
+  if(p.load() != nullptr)
+  {
+    if (p.load()->isLeaf)
+      cout<<" "<<p.load()->data<<" ";
+    if(p.load()->left.load())
+      preorder(p.load()->left);
+    if(p.load()->right.load())
+      preorder(p.load()->right);
+  }
+  else return;
 }
 
--- SequentialBST.cpp
+++ SequentialBST.cpp
@@ -12,34 +12,23 @@ using namespace std;
 void SequentialBST::insert(int k)
 {
     searchResult * s = search(k);
-    tnp newinternal;
-    newinternal.store(s->l);
-    if(newinternal.load()->data == k){
+    tnp newInternal;
+    newInternal.store(s->l);
+    if(newInternal.load()->data == k){
         return;
     }
-    tnp newnode;
-    newnode.store(new tree_node);
-    newnode.load()->data = k;
-    newnode.load()->left.store(nullptr);
-    newnode.load()->right.store(nullptr);
-    newnode.load()->isLeaf.store(true);
-   
-    tnp newsibling;
-    newsibling.store(new tree_node);
-    newsibling.load()->data = newinternal.load()->data;
-    newsibling.load()->left.store(nullptr);
-    newsibling.load()->right.store(nullptr);
-    newsibling.load()->isLeaf.store(true);
+    treeNode * newNode = new treeNode(k, true);
+    treeNode * newSibling = new treeNode(newInternal.load()->data, true);
     
-    newinternal.load()->data = max(k,newinternal.load()->data);
-    newinternal.load()->isLeaf.store(false);
-    if(k < newinternal.load()->data){
-        newinternal.load()->left.store(newnode);
-        newinternal.load()->right.store(newsibling);
+    newInternal.load()->data = max(k,newInternal.load()->data);
+    newInternal.load()->isLeaf = false;
+    if(k < newInternal.load()->data){
+        newInternal.load()->left.store(newNode);
+        newInternal.load()->right.store(newSibling);
     }
     else{
-        newinternal.load()->left.store(newsibling);
-        newinternal.load()->right.store(newnode);
+        newInternal.load()->left.store(newSibling);
+        newInternal.load()->right.store(newNode);
     }
 }
 
